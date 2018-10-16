6acc8ecda675684b64e4c7c2f53edcee5371b9b6
Enrique Bris <enrique.bris@creativedrive.com>
2018-10-16 19:24:43 -0400

    [pool] - examples

--- examples/complex_jobs/main.go
+++ examples/complex_jobs/main.go
@@ -21,20 +21,20 @@ type JobData struct {
 func main() {
 	// total workers
 	totalWorkers := 10
-	// max number of tasks waiting in the channel
-	maxNumberJobsInChannel := 15
+	// max number of pending jobs
+	maxNumberPendingJobs := 15
 	// do not show messages about the pool processing
 	verbose := false
 
-	pool := goworkerpool.NewPool(totalWorkers, maxNumberJobsInChannel, verbose)
+	pool := goworkerpool.NewPool(totalWorkers, maxNumberPendingJobs, verbose)
 
-	// add the worker function
+	// add the worker handler function
 	pool.SetWorkerFunc(worker)
 
 	// start up the workers
 	pool.StartWorkers()
 
-	// add tasks in a separate goroutine
+	// enqueue jobs in a separate goroutine
 	go func() {
 		for i := 0; i < 30; i++ {
 			// the jobs contain a custom struct: JobData
@@ -52,7 +52,7 @@ func main() {
 	pool.Wait()
 }
 
-// worker is the function each pool's worker will invoke to process jobs
+// worker is the function each worker will invoke to process jobs
 func worker(data interface{}) bool {
 	// cast the interface{} back to our custom JobData
 	if job, ok := data.(JobData); ok {
--- examples/exit_after_n_jobs_get_successfully_processed/main.go
+++ examples/exit_after_n_jobs_get_successfully_processed/main.go
@@ -3,7 +3,7 @@
 //  - 100 jobs will be enqueued
 //  - all workers will exit after 30 jobs are successfully processed
 
-package exit_after_n_jobs_get_successfully_processed
+package main
 
 import (
 	"log"
@@ -15,14 +15,14 @@ import (
 func main() {
 	// total workers
 	totalWorkers := 10
-	// max number of tasks waiting in the channel
-	maxNumberJobsInChannel := 15
+	// max number of pending jobs
+	maxNumberPendingJobs := 15
 	// do not show messages about the pool processing
 	verbose := false
 
-	pool := goworkerpool.NewPool(totalWorkers, maxNumberJobsInChannel, verbose)
+	pool := goworkerpool.NewPool(totalWorkers, maxNumberPendingJobs, verbose)
 
-	// add the worker function
+	// add the worker handler function
 	pool.SetWorkerFunc(func(data interface{}) bool {
 		log.Printf("processing %v\n", data)
 		// add a 1 second delay (to makes it look as it were processing the job)
@@ -36,13 +36,13 @@ func main() {
 	// start up the workers
 	pool.StartWorkers()
 
-	// add tasks in a separate goroutine
+	// enqueue jobs in a separate goroutine
 	go func() {
 		for i := 0; i < 100; i++ {
 			pool.AddTask(i)
 		}
 	}()
 
-	// wait while at least one worker is alive
+	// wait until 30 jobs get successfully processed
 	pool.WaitUntilNSuccesses(30)
 }
